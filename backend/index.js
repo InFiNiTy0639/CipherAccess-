3aec5ac669389975b199410cd09b2933e4b26b9e
marq <classicvector63@gmail.com>
2024-10-19 14:59:25 +0530

    Implemented full auth  form with validation

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./db/connectDB.js";
-import authRoutes from "./routes/auth.route.js"
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(express.json());// allows us to parse incoming requests:req.body
-app.use(cookieParser()); //for parse incoming cookies
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-// app.get("/", (req, res) => {
-//   res.send("Hi there");
-// });
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 // 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
@@ -27,6 +28,6 @@ app.use("/api/auth",authRoutes)
 // }
 
 app.listen(PORT, () => {
-  connectDB();
-  console.log("server is running on the port:",PORT);
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
